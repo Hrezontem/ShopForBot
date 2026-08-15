fcba2a863611abcd3b8d7e111a77a12aff1aaf7c
Tarapata Nikita <hrezontem@yandex.ru>
2026-08-16 00:31:30 +0300

    refactor(bot): CallbackData классы пернесены в отделньный файл

---
+++ handlers/callback_data.py
@@ -0,0 +1,15 @@
+from aiogram.filters.callback_data import CallbackData
+
+
+class ProductClickData(CallbackData, prefix="prod"):
+    id: int
+
+class AddToCartData(CallbackData, prefix="addcart"):
+    variant_id: int
+
+class DeleteCartItemData(CallbackData, prefix="del_cart_item"):
+    cart_item_id: int
+
+
+class ChangeCartItemData(CallbackData, prefix="change_cart_item"):
+    cart_item_id: int
