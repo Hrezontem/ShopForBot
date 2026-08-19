b18525ba4d32747e59e8f33c62024d78fc1fa525
Tarapata Nikita <hrezontem@yandex.ru>
2026-08-19 15:21:53 +0300

    feat(bot): файл со стейтами для fsm

---
+++ handlers/states.py
@@ -0,0 +1,15 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class ProfileStates(StatesGroup):
+    last_name = State()
+    first_name = State()
+    mid_name = State()
+    phone = State()
+    address = State()
+    email = State()
+
+
+class OrderStates(StatesGroup):
+    delivery_method = State()
+    comment = State()
